c3d3a0c01c5fb901147fe104e3e88b98579f522a
apple <p.ppraeww@hotmail.com>
2016-10-19 18:05:09 +0700

    Initial commit

---
+++ testfunc.js
@@ -0,0 +1,10 @@
+/**
+ * Created by apple on 10/18/2016 AD.
+ */
+function hi(){
+    window.alert("Hellooo");
+}
+function anyeong(){
+    document.getElementById("nn").innerHTML = document.getElementById("name");
+}
+
